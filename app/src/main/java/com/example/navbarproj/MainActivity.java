85df4ca4480da777539f4447be949819a4864b19
Ausree Ghose <ausreeghose97@gmail.com>
2019-07-08 00:14:47 +0530

    Project

---
+++ app/src/main/java/com/example/navbarproj/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.navbarproj;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout dl;
+    NavigationView nv;
+    TextView tv;
+    ActionBarDrawerToggle toggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initialize views from the layout
+        dl = findViewById(R.id.drawerLayout);
+        nv = findViewById(R.id.navigationView);
+        tv = findViewById(R.id.textView);
+
+        //ActionBarDrawerToggle is initialized to sync drawer open and closed states
+        toggle = new ActionBarDrawerToggle(this, dl, R.string.open_menu, R.string.close_menu);
+
+        dl.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //The Hamburger icon is applied to the action bar for working with the nav drawer
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //On clicking of any menu items, actions will be performed accordingly
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.temp:
+                        startActivity(new Intent(MainActivity.this, MainActivity.class));
+                        break;
+                    case R.id.wind:
+                        startActivity(new Intent(MainActivity.this, WindActivity.class));
+                        break;
+                    case R.id.sun:
+                        startActivity(new Intent(MainActivity.this, SunActivity.class));
+                        break;
+                    case R.id.details:
+                        startActivity(new Intent(MainActivity.this, DetailsActivity.class));
+                        break;
+                    default: return(true);
+                }
+                return true;
+            }
+        });
+    }
+
+    //On clicking any Menu Item this function is called and it returns the ID to the main function
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(toggle.onOptionsItemSelected(item))
+            return true;
+        else if(item.getItemId()==R.id.exit) {
+            finishAffinity();
+            System.exit(0);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.new_menu, menu);
+        return true;
+    }
+
+}
